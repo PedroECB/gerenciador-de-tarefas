96f348e6314f24b036315d850f306a95564c4b8c
Pedro Bastos <pedroxdman@hotmail.com>
2018-02-13 22:37:36 -0300

    Adicionado bootstrap na parte de edicao

--- editar.php
+++ editar.php
@@ -1,34 +1,52 @@
 <?php
 
 include_once "servidor.php"; 
-
+if(!empty($_GET['id'])){
 $id = $_GET['id'];
 
 $vetor = consulta2(intval($_GET['id']));
+foreach ($vetor as $row) {
+  $nome = $row['tarefa'];
+  $descricao = $row['descricao'];
+  $concluida = $row['concluida'];
+}
+
+}
+
+
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Editar</title>
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
   <link rel="stylesheet" href="estilo/normalize.css">
   <link rel="stylesheet" href="estilo/estilo2.css">
 </head>
+
+</div>
 <body>
+  <div class="col-md-4">
+    
+  </div>
     <form>
-      <fieldset><legend>Editar</legend>
+      <fieldset>
+        <h2>Editar</h2>
          <b>Tarefa:</b><br>
-         <input type="text" name="nome" class="nome"><br>
+         <input type="text" name="nome" class="nome form-control" value="<?php echo $nome;?>"><br>
 
          <br><b>Descrição</b><br>
-         <textarea name="descricao" cols="25" rows="2" maxlength="75" class="nome"></textarea><br>
+         <textarea name="descricao" cols="25" rows="2" maxlength="75" class="nome form-control"><?php echo $descricao; ?></textarea><br>
 
          <br><b>Prazo</b><br>
-         <input type="date" name="prazo" value=""><br>
+         <input type="date" name="prazo" value="" class="form-control"><br>
 
          <br><b>Prioridade</b>
 
-         <select name="prioridade">
+         <select name="prioridade" class="form-control">
            <option value="1">Baixa</option>
            <option value="2">Média</option>
            <option value="3">Alta</option>  
@@ -36,8 +54,10 @@ $vetor = consulta2(intval($_GET['id']));
           
           <br>
          <br><b>Concluída</b>
-         <input type="checkbox" name="concluida" value="1" placeholder="">
-         <input type="button" value="Salvar">
+         <input type="checkbox" name="concluida" value="2" placeholder=""<?php if($concluida == 1){ echo "checked";}?>>
+         <br>
+         <br>
+         <input type="submit" value="Salvar" class="btn btn-primary">
 
       </fieldset>
 
