e3f088f6d0e27453573359f85f76b3d193769f48
Pedro Bastos <pedroxdman@hotmail.com>
2018-02-13 21:33:09 -0300

    Adicionado bootstrap na página principal

--- index.php
+++ index.php
@@ -13,11 +13,18 @@
 <head>
   <title>Agenda</title>
   <meta charset="utf-8">
+  <meta name=viewport content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
   <link rel="stylesheet" type="text/css" href="estilo/normalize.css">
   <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
 </head>
+<div class="page-header">
+  
+</div>
 <body>
-  <table>
+  <div class="container">
+  <table class="table table-condensed table-bordered table-responsive">
     <thead class="cabeca">
       <tr>
         <th>Número</th>
@@ -30,22 +37,22 @@
 </thead>
       <tr class="enter2">
         <form action="index.php">
-        <td><input class="enter" type="text" size="" name="numero" maxlength="4" autofocus=""></td>
-        <td><input class="enter" type="text" size="" name="tarefa"></td>
-        <td><input class ="enter" type="date" name="prazo" placeholder="DD/MM/AAAA"></td>
-        <td><input class="enter" type="text" size="" name="descricao"></td>
+        <td><input class="enter form-control" type="text" size="" name="numero" maxlength="4" autofocus=""></td>
+        <td><input class="enter form-control" type="text" size="" name="tarefa"></td>
+        <td><input class ="enter form-control" type="date" name="prazo" placeholder="DD/MM/AAAA"></td>
+        <td><input class="enter form-control" type="text" size="" name="descricao"></td>
         <td><input type="radio" name="concluida" value="1"><b>Sim</b><br><input type="radio" name="concluida" value="2"><b>Não</b></td>
-        <td><input class="btnn1" type="submit" value="Agendar" id="cadastrar"></td>
+        <td><input class="btn btn-warning btn-block" type="submit" value="Agendar" id="cadastrar"></td>
        </form> 
       </tr>
     
     <tr class="tr1">
-      <td>0789</td>
+      <td><b>0789</b></td>
       <td>Estudar Java</td>
       <td>05/03/2018</td>
       <td>Estudar Java para certificação Oracle</td>
       <td>Não</td>
-      <td><input class="botoes" type="button" value="Editar"><input class="botoes" type="button" value="Excluir"></td>
+      <td><input class="button btn btn-info btn-sm" type="button" value="Editar"><input class="button btn btn-danger btn-sm" type="button" value="Excluir"></td>
     </tr>
 
     <?php $cont =0; foreach($vetor as $row): $cont++; ?>
@@ -53,15 +60,15 @@
 
 
      <tr class="<?php echo $class;?>">
-      <td><?php echo $row['numero']; ?></td>
+      <td><b><?php echo $row['numero']; ?></b></td>
       <td><?php echo $row['tarefa']; ?></td>
       <td><?php echo traduzData($row['prazo']); ?></td>
       <td><?php echo $row['descricao']; ?></td>
       <td><?php echo traduzConcluida($row['concluida']); ?></td>
       <td class="opcoes">
 
-        <a href="editar.php?id=<?php echo $row['id'];?>"><input class="botoes btnn2" type="button" value="Editar"></a>
-        <input class="botoes btnn3" type="button" value="Excluir">
+        <a href="editar.php?id=<?php echo $row['id'];?>"><input class="button btn btn-info btn-sm" type="button" value="Editar"></a>
+        <input class="button btn btn-danger btn-sm" type="button" value="Excluir">
       </td>
        
      </tr>    
@@ -71,6 +78,8 @@
 
 
 
-  </table>
+    </table>
+ </div> 
+
 </body>
 </html>
